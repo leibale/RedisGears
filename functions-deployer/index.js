e24b9a94eb025f38b67cc87429112c13c619d66b
leibale <leibale1998@gmail.com>
2022-07-26 11:12:27 -0400

    JavaScript function-deployer

---
+++ functions-deployer/index.js
@@ -0,0 +1,63 @@
+import { Command } from 'commander';
+import { createClient } from '@redis/client';
+import { join } from 'path';
+import * as rollup from 'rollup';
+import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
+
+await new Command()
+  .argument('<filename>')
+  .option('-r, --redis')
+  .option('-w, --watch')
+  .action(async (filename, { redis, watch }) => {
+    const client = createClient({ url: redis });
+    client.on('error', err => console.error('Redis client error', err));
+    await client.connect();
+
+    return (watch ? watchAndDeploy : buildAndDeploy)(client, {
+      input: {
+        file: join(process.cwd(), filename)
+      },
+      plugins: [
+        preserveShebangs()
+      ]
+    });
+  })
+  .parseAsync();
+
+async function buildAndDeploy(client, rollupOptions) {
+  const result = await rollup.rollup(rollupOptions);
+  try {
+    await deploy(client, await result.generate({}));
+  } finally {
+    await client.quit();
+  }
+}
+
+async function watchAndDeploy(client, rollupOptions) {
+  (await rollup.watch({
+    ...rollupOptions,
+    watch: {
+      skipWrite: true
+    }
+  })).on('event', async ({ code, error, result }) => {
+    switch (code) {
+      case 'ERROR':
+        console.error('Rollup bundle error', error);
+        break;
+
+      case 'BUNDLE_END':
+        await deploy(client, await result.generate({}));
+        break;
+    }
+  });
+}
+
+async function deploy(client, { output: [{ code }] }) {
+  console.log('Deplying...');
+  try {
+    await client.sendCommand(['RG.FUNCTION', 'LOAD', 'UPGRADE', code]);
+    console.log('Deployed! :)');
+  } catch (err) {
+    console.error('Deploy error', err);
+  }
+}
